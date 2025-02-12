8dc5153da859d2d11b2cce5c419d6c7ee14e918e
CarlosGalaron <carlos.galaron@gmail.com>
2025-02-12 11:00:53 +0100

    entrega

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { EmpleListComponent } from './emple-list/emple-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/emple-list', element EmpleListComponent },
+    { path: '/emple-create', element EmpleListComponent },
+    
+];
